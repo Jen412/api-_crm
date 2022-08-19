ea2cd3a3f0a258e4a10efedf413faa7e904b56da
Jen412 <72884006+Jen412@users.noreply.github.com>
2022-08-18 23:00:43 -0500

    cambios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -50,7 +50,7 @@ const Formulario = ({cliente, cargando}) => {
                 })
             }
             await respuesta.json();
-            navigate('/clientes')
+            navigate('/')
         } catch (error) {
             console.log(error)
         }
